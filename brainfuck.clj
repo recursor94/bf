834b9cbb3ca4d81b24dfff4730f9c6c4b99b8b33
Andrew Spano <andrew.spano1@marist.edu>
2013-04-04 17:42:07 -0400

    added descriptive comment at the top of the source code.

--- brainfuck.clj
+++ brainfuck.clj
@@ -1,11 +1,15 @@
-;a lazy vector to represent the brainfuck memory cell.
+;;;This is an interpreter for the brainfuck programming language written in
+;;;clojure.  I tried to abstract out as much functionality as I could
+;;;into compact functions with a single specific purpose.  Not everything in
+;;:this code may be entirely idiomatic.
+;;Andrew Spano
 
 
-(declare begin-loop end-loop)
+(declare begin-loop end-loop) ;;declare the functions up here to be called where needed. The only reason this line is here is because the looping functions are not.
 
-(def cells (atom (vec (repeat 9001 0))))
+(def cells (atom (vec (repeat 9001 0)))) ;a lazy vector to represent the brainfuck memory cell.
 
-(def pointer (atom 0))
+(def pointer (atom 0))  ;;the mutable pointer that will 
 
 (defn plus
   "(+) Increments current memory cell"
