8f2ee4b3a23b091fda91c83b620f85e9c62ae430
Andrew Spano <andrew.spano1@marist.edu>
2013-04-06 19:10:19 -0400

    got rid of  all traces

--- brainfuck/src/brainfuck/fuck.clj
+++ brainfuck/src/brainfuck/fuck.clj
@@ -131,20 +131,19 @@ index holds the current position of the interpreter in its execution."
   "executes each function in the codemap vector in sequential order"
 
 
-    ([end-index]
+  ([end-index]
        (inc-code-pos)
-       (trace
-        (loop [index (:index @codemap)]
-          (let [codevec (@codemap :struct)
-                instruct (get codevec index)]
-            (println "index:" index
-                     "instruct" instruct
-                     "minus one index:" (- end-index 2))
-            (instruct))
-          (when-not (= index (- end-index 1))
-            (println "yeah you are")
-            (inc-code-pos)
-            (recur (inc index))))))
+       (loop [index (:index @codemap)]
+         (let [codevec (@codemap :struct)
+               instruct (get codevec index)]
+           (println "index:" index
+                    "instruct" instruct
+                    "minus one index:" (- end-index 2))
+           (instruct))
+         (when-not (>= index (- end-index 1))
+           (println "yeah you are")
+           (inc-code-pos)
+           (recur (inc index)))))
     ([]
        (doseq [instruct (@codemap :struct)]
          (instruct) ;;higher order functions ftw
